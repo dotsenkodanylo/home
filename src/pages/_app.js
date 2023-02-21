200acbe7d5eaee7e221291021394bd476f1309b5
Danylo Dotsenko <dotsenkodanylo@gmail.com>
2023-02-21 09:01:37 -0500

    Initial commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,12 @@
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <main className='page'>
+        {/* Add header here for upper nav bar -> will need to adjust height correctly */}
+        <Component {...pageProps} />
+      </main>
+    </>
+  );
 }
